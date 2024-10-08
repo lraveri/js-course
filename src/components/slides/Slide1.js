32d1c8026d8a8badbae77aa54611db6c257afa78
Luca Raveri <lucaraveri993@gmail.com>
2024-10-08 15:24:05 +0200

    feat: slides (wip)

--- src/components/slides/Slide1.js
+++ src/components/slides/Slide1.js
@@ -1,5 +1,7 @@
 export default function Slide1() {
     return (
-        <h1 className="text-4xl font-bold text-white">Welcome to my presentation!</h1>
+        <div className="flex items-center justify-center h-full">
+            <h1 className="text-4xl font-bold text-white">Welcome to my presentation!</h1>
+        </div>
     );
 }
